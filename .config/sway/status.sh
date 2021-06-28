47f4ff5490f4c129a659feb2ce517166d264c95c
Oleh Hushchenkov <o.hushchenkov@gmail.com>
2021-06-28 12:37:25 +0300

    Update

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 tp=$(sysctl dev.cpu | grep temperature | cut -f 2 -d " " | paste -s -d " " - | xargs echo "CPU")
+bl=$(backlight -q | xargs echo 'Brightness')
 vl=$(mixer -s pcm | cut -f 1- -d " " | xargs echo "Mixer")
 bt=$(acpiconf -i 0 | grep "Remaining capacity" | paste -s -d " " - | xargs echo)
 dt=$(date +'%Y-%m-%d %H:%M:%S')
 
-echo "$tp | $vl | $bt | $dt"
+echo "$tp | $bl | $vl | $bt | $dt"

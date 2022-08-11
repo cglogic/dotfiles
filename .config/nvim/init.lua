6c33d8e217fd793ba4c0f2423df89625560fb4e3
Oleh Hushchenkov <o.hushchenkov@gmail.com>
2022-08-11 22:24:12 +0300

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,6 +24,7 @@ require "paq" {
 	'neovim/nvim-lspconfig';
 	'hrsh7th/cmp-nvim-lsp';
 	-- 'hrsh7th/cmp-buffer';
+	'L3MON4D3/LuaSnip';
 	'hrsh7th/nvim-cmp';
 
 	'ibhagwan/fzf-lua';
@@ -154,7 +155,6 @@ win.default_opts = function(options)
 	return opts
 end
 
-------------------------------------------------------------
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
 	vim.lsp.handlers.hover,
 	{border = 'rounded'}
@@ -165,6 +165,13 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
 	{border = 'rounded'}
 )
 
+------------------------------------------------------------
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 local cmp = require 'cmp'
 cmp.setup({
 	window = {
@@ -180,7 +187,8 @@ cmp.setup({
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		-- { name = "luasnip" },
+	}, {
+		{ name = "luasnip" },
 	-- }, {
 		-- { name = "buffer" },
 	}),
@@ -190,26 +198,48 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
 	formatting = {
 		format = function(entry, item)
 			-- item.kind = lsp_symbols[item.kind]
-			-- item.abbr = string.sub(item.abbr, 1, 20)
+			item.abbr = string.sub(item.abbr, 1, 50)
 			item.menu = ({
 				-- buffer = "[Buffer]",
 				nvim_lsp = "[LSP]",
-				-- luasnip = "[Snippet]",
+				luasnip = "[Snippet]",
 				-- neorg = "[Neorg]",
 			})[entry.source.name]
 
 			return item
 		end,
 	},
-	-- snippet = {
-	-- 	expand = function(args)
-	-- 		luasnip.lsp_expand(args.body)
-	-- 	end,
-	-- },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 })
 
 -------------------- COMMANDS ------------------------------

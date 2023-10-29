17b259ef748999dd9bcdf38c17ad688ee87bc933
Oleh Hushchenkov <o.hushchenkov@gmail.com>
2023-10-29 21:07:22 +0200

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -149,7 +149,13 @@ require('nvim-treesitter.configs').setup {
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
-	}
+	},
+	incremental_selection = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
 }
 
 -------------------- LSP -----------------------------------

8fd8b50c866c311ed641b4a59a5c1cb79c3ee977
Oleh Hushchenkov <o.hushchenkov@gmail.com>
2026-06-11 20:24:40 +0300

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,79 +15,11 @@ g.mapleader = ','
 
 local border = 'single'
 
--------------------- PLUGINS -------------------------------
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	})
-end
-opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-	'folke/lazy.nvim',
-
-	'RRethy/nvim-base16',
-	'wincent/base16-nvim',
-	-- 'rebelot/kanagawa.nvim',
-	-- 'folke/tokyonight.nvim',
-	-- 'ellisonleao/gruvbox.nvim',
-	-- 'sainnhe/gruvbox-material',
-	-- 'AlexvZyl/nordic.nvim',
-
-	-- 'kyazdani42/nvim-web-devicons',
-	'hoob3rt/lualine.nvim',
-	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-
-	'neovim/nvim-lspconfig',
-	'hrsh7th/cmp-nvim-lsp',
-	-- 'hrsh7th/cmp-buffer',
-	'hrsh7th/nvim-cmp',
-	-- 'glepnir/lspsaga.nvim',
-
-	'ibhagwan/fzf-lua',
-	-- 'lewis6991/spaceless.nvim',
-	-- 'terrortylor/nvim-comment',
-	-- 'Everduin94/nvim-quick-switcher',
-}, {
-	ui = {
-		border = border,
-		icons = {
-			cmd = '',
-			config = '',
-			event = '',
-			ft = '',
-			init = '',
-			keys = '',
-			plugin = '',
-			runtime = '',
-			source = '',
-			start = '',
-			task = '',
-			lazy = '',
-		},
-	},
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-	desc = 'User: fix backdrop for lazy window',
-	pattern = 'lazy_backdrop',
-	group = vim.api.nvim_create_augroup('lazynvim-fix', { clear = true }),
-	callback = function(ctx)
-		local win = vim.fn.win_findbuf(ctx.buf)[1]
-		vim.api.nvim_win_set_config(win, { border = 'none' })
-	end,
-})
-
 -------------------- OPTIONS -------------------------------
 opt.modeline = false                   -- Disable modeline
 opt.background = 'dark'           -- or "light" for light mode
-cmd 'colorscheme base16-tomorrow-night'-- Put your favorite colorscheme here
+cmd 'colorscheme retrobox'
+-- cmd 'colorscheme base16-tomorrow-night'-- Put your favorite colorscheme here
 -- cmd 'colorscheme tomorrow-night'    -- Put your favorite colorscheme here
 opt.winborder = border
 -- opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options
@@ -120,6 +52,8 @@ end
 
 vim.api.nvim_command [[match errorMsg /\s\+$/]]
 
+-- vim.api.nvim_set_hl(0, "Delimiter", { fg = "#0000ff" })
+
 -------------------- MAPPINGS ------------------------------
 map('n', ';', ':')  -- Map ; -> :
 
@@ -139,96 +73,6 @@ map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 -- map("i", "<C-j>", "<Esc>:m .-2<CR>==gi", cmd_options)
 
 ------------------------------------------------------------
-
-require('lualine').setup {
-	options = {
-		icons_enabled = false,
-		-- theme = 'tokyonight',
-		section_separators = '',
-		component_separators = '',
-	}
-}
-
--------------------- TREE-SITTER ---------------------------
-require('nvim-treesitter.configs').setup {
-	-- ensure_installed = 'maintained',
-	ensure_installed = {
-		'c',
-		'cpp',
-		'python',
-		'lua',
-		'sql',
-		'diff',
-		'json',
-		'xml',
-		'yaml',
-		'markdown',
-		'regex',
-		'gn',
-		'ninja',
-		'meson',
-		'cmake',
-		'make',
-		'vim',
-		'vimdoc',
-		'gitcommit',
-		'muttrc',
-	},
-	auto_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	incremental_selection = {
-		enable = true,
-	},
-	indent = {
-		enable = true,
-	},
-}
-
--------------------- LSP -----------------------------------
--- local lsp = require 'lspconfig'
-
--- Setup lspconfig.
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities(
-	vim.lsp.protocol.make_client_capabilities()
-)
-
-local function on_attach()
-	-- Hide semantic highlights for functions
-	vim.api.nvim_set_hl(0, '@lsp.type.function', {})
-	-- Hide all semantic highlights
-	for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-		vim.api.nvim_set_hl(0, group, {})
-	end
-end
-
-vim.lsp.config('ccls', {
-	on_attach = on_attach(),
-	init_options = {
-		cache = {
-			directory = os.getenv('XDG_RUNTIME_DIR') .. '/ccls-cache';
-		};
-	},
-	autostart = false,
-	capabilities = lsp_capabilities,
-})
--- vim.lsp.enable({'ccls'})
-
--- vim.lsp.config('clangd', {
--- 	on_attach = on_attach(),
--- 	cmd = {
--- 		"clangd19",
--- 		"--background-index",
--- 		"--header-insertion=never",
--- 		-- "--suggest-missing-includes",
--- 	},
--- 	autostart = false,
--- 	capabilities = lsp_capabilities,
--- }
--- vim.lsp.enable({'clangd'})
-
 vim.keymap.set('n', '<space>,', function()
   vim.diagnostic.jump({ count = -1, float = true })
 end)
@@ -259,119 +103,13 @@ vim.diagnostic.config {
 	},
 }
 
-local win = require('lspconfig.ui.windows')
-local _default_opts = win.default_opts
-
-win.default_opts = function(options)
-	local opts = _default_opts(options)
-	opts.border = border
-	return opts
-end
-
 vim.lsp.buf.hover({ border = border })
 
-------------------------------------------------------------
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local cmp = require('cmp')
-cmp.setup({
-	window = {
-		documentation = {
-			border = border,
-		},
-		completion = {
-			border = border,
-		},
-	},
-	completion = {
-		autocomplete = false
-	},
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		-- { name = "buffer" },
-	}),
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-		['<C-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-
-		['<C-S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	}),
-	formatting = {
-		format = function(entry, item)
-			-- item.kind = lsp_symbols[item.kind]
-			item.abbr = string.sub(item.abbr, 1, 50)
-			item.menu = ({
-				-- buffer = '[Buffer]',
-				nvim_lsp = '[LSP]',
-			})[entry.source.name]
-
-			return item
-		end,
-	},
-})
-
 -------------------- COMMANDS ------------------------------
+
 cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
 
 --------------------------------------------------------------------
-local actions = require 'fzf-lua.actions'
-require('fzf-lua').setup {
-	files = {
-		previewer = false,
-		cmd = 'ag --nocolor -l -g "" | sort',
-		find_opts = '',
-		git_icons = false,
-		file_icons = false,
-	},
-	grep = {
-		previewer = false,
-		prompt = '❯ ',
-		cmd = 'ag --nocolor',
-		grep_opts = '',
-	},
-	buffers = {
-		previewer = false,
-	},
-	tabs = {
-		previewer = false,
-	},
-	lines = {
-		previewer = false,
-	},
-	winopts = {
-		border = border,
-	}
-}
-------------------------------------------------------------
--- require('spaceless').setup {}
-
-------------------------------------------------------------
-vim.keymap.set('n', '<Leader>f', '<cmd>lua require("fzf-lua").files({hidden=false})<CR>')
-vim.keymap.set('n', '<Leader>b', '<cmd>lua require("fzf-lua").buffers()<CR>')
-vim.keymap.set('n', '<Leader>g', '<cmd>lua require("fzf-lua").grep()<CR>')
--- vim.keymap.set('n', '<Leader>s', '<cmd>lua require("nvim-quick-switcher").toggle("cpp", "h")<CR>')
 
 vim.keymap.set("n", "<leader>o", ":copen<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>c", ":cclose<CR>", { noremap = true, silent = true })
